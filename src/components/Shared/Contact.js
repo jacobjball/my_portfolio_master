013a2089afa4a6ed98be239a6d189c5e16b9f801
Jake Ball <jacobjball18@gmail.com>
2021-09-27 22:09:27 -0600

    added content to about and contact page

--- src/components/Shared/Contact.js
+++ src/components/Shared/Contact.js
@@ -2,9 +2,21 @@ import React from 'react';
 import '../../index.css'
 
 const Contact = () => (
-  <>
-    <div class='headertext'> Contact </div>
-  </>
+  <div>
+    <div class='row'>
+      <div class='column'>
+        <div class='abouttext'>
+          <div>
+            <a class='btn btn-outline' href= "mailto: jacobjball18@gmail.com"> Send an Email </a>
+          </div>
+          <br/>
+          <div>
+            <a class='btn btn-outline' href='tel:+13857227216'> Call Me </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 )
 
 export default Contact;
